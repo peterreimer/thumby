7e33825a75e557cd62b91162a93ea964a13b0a32
Jan Schnasse <jan@schnasse.eu>
2015-12-11 11:26:43 +0100

    Add some tests
    - change creation of tmp files so, that original name is kept

--- test/tests/ApplicationTest.java
+++ test/tests/ApplicationTest.java
@@ -1,12 +1,27 @@
+package tests;
+
 import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.contentAsString;
 import static play.test.Helpers.contentType;
 import static play.test.Helpers.fakeApplication;
 import static play.test.Helpers.running;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import models.Thumbnail;
 
 import org.junit.Test;
 
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
+import com.google.common.io.Files;
+import com.google.common.net.MediaType;
+
+import play.Play;
 import play.mvc.Content;
 
 /**
@@ -18,18 +33,30 @@ import play.mvc.Content;
 public class ApplicationTest {
 
     @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
-    }
-
-    @Test
-    public void indexTemplateShouldContainTheStringThatIsPassedToIt() {
+    public void test_thumbnailer() {
         running(fakeApplication(), new Runnable() {
             public void run() {
-                Content html = views.html.index.render(new Thumbnail());
-                assertThat(contentType(html)).isEqualTo("text/html");
-                assertThat(contentAsString(html)).contains("Supports PDF and Images.");
+                 org.junit.Assert.assertEquals("63ae2f7551277468be0f416c9267030d",createTestThumbnail("test.jpg",MediaType.JPEG));
+                 org.junit.Assert.assertEquals("a1c75531df7039c6515b17e5378b8bb5",createTestThumbnail("test.png",MediaType.PNG));
+                 org.junit.Assert.assertEquals("8c58ffea16b7cab3794221288e66c733",createTestThumbnail("test.zip",MediaType.ZIP));
+                 org.junit.Assert.assertEquals("e29aeb2854ec56fdf358645fc53440d1",createTestThumbnail("test.ods",MediaType.OPENDOCUMENT_SPREADSHEET));
+                 org.junit.Assert.assertEquals("b89f887968762d629c193ae25db02e05",createTestThumbnail("test.pdf", MediaType.PDF));
+                 org.junit.Assert.assertEquals("740d6aeff9cd1a6263890cc5e758e332",createTestThumbnail("test.xcf",MediaType.ANY_IMAGE_TYPE));
+                 org.junit.Assert.assertEquals("b856b3d7b882f25d0986416ac1aa0102",createTestThumbnail("test.gif",MediaType.GIF));
+            }
+
+            private String createTestThumbnail(String name, MediaType mtype) {
+                try {
+                    Thumbnail t = controllers.Application.createThumbnail(
+                            new FileInputStream(
+                                    new File(Play.application().resource( name).toURI().getPath())),
+                            mtype, 200, name);
+                    HashCode hashcode= Files.hash(t.thumb, Hashing.md5());
+                    play.Logger.info("Create Test Data at " + t.thumb.getAbsolutePath()+" Hashcode: "+hashcode);
+                    return hashcode.toString();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
     }
